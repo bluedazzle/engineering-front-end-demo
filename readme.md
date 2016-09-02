0689548ada9e9589d2e19688b8a1e35e676d2021
rapospectre <rapospectre@163.com>
2016-09-02 20:12:03 +0800

    <feat>  finish front-end content
    Author:rapospectre@gmail.com
    
    Desc: a standrad front-end engineering project template

--- readme.md
+++ readme.md
@@ -1,3 +1,19 @@
 ## 前端工程化学习
 
-### test
+### webpack server hot module replacement
+
+```
+    $ webpack-dev-server --inline --hot
+```
+or
+
+add HotModuleReplacementPlugin() in plugins:
+
+```
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        hot: true,
+        inline: true}
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,64 @@
-var webpack = require('webpack')
-module.exports = {
-  entry: ['./src/index.js', './src/style.css'],
+var webpack = require('webpack');
+const glob = require('glob');
+
+var config = {
+  entry: {
+    vendor: [
+      'react',
+      'react-dom'
+    ]
+  },
   output: {
-    path: './dist/',
-    filename: 'index.js'
+    path: __dirname + '/dist/js/',
+    filename: '[name].js'
   },
   module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015', 'stage-0', 'react']
-            }
-        },
-        {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            loader: 'style-loader!css-loader'
-        }]
+    preLoaders:[{
+        test: /\.js$/, 
+        loader: "eslint-loader", 
+        exclude: /node_modules/
+  }],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: [ 'es2015', 'stage-0', 'react' ] }
+      }
+    ]
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        hot: true,
+        inline: true
     }
+};
+
+/**
+ * find entries
+ */
+var files = glob.sync('./src/js/*/index.js');
+var newEntries = files.reduce(function(memo, file) {
+  var name = /.*\/(.*?)\/index\.js/.exec(file)[1];
+  memo[name] = entry(name);
+  return memo;
+}, {});
+
+config.entry = Object.assign({}, config.entry, newEntries);
+
+/**
+ * [entry description]
+ * @param  {[type]} name [description]
+ * @return {[type]}      [description]
+ */
+function entry(name) {
+  return './src/js/' + name + '/index.js';
 }
+
+module.exports = config;
